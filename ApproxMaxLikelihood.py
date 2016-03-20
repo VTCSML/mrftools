fixed some bugs in approx max likelihood and energy functional computation. Added log-linear model classes for models with conditional, continuous features for unary potentials.

ApproxMaxLikelihood.py
@@ -3,7 +3,7 @@
 from MarkovNet import MarkovNet
 from BeliefPropagator import BeliefPropagator
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize, check_grad
 
 class ApproxMaxLikelihood(object):
     """Object that runs approximate maximum likelihood parameter training."""
@@ -85,10 +85,10 @@ class ApproxMaxLikelihood(object):
         for i in range(len(self.potentials)):
             if isinstance(self.potentials[i], tuple):
                 # get pairwise belief
-                table = self.bp.pairBeliefs[self.potentials[i]]
+                table = np.exp(self.bp.pairBeliefs[self.potentials[i]])
             else:
                 # get unary belief
-                table = self.bp.varBeliefs[self.potentials[i]]
+                table = np.exp(self.bp.varBeliefs[self.potentials[i]])
 
             # flatten table and append
             marginals.extend(table.reshape((-1, 1)).tolist())
@@ -98,7 +98,7 @@ class ApproxMaxLikelihood(object):
 
 
     def objective(self, weightVector):
-        """Compute the learning objective with the provided weight vector."""
+        """Compute the learning objective with the provided weight vector. Approximate negative log-likelihood."""
         self.setWeights(weightVector)
         marginals = self.getMarginalVector()
 
@@ -108,8 +108,10 @@ class ApproxMaxLikelihood(object):
         objective += self.l1Regularization * np.sum(np.abs(weightVector))
         objective += 0.5 * self.l2Regularization * weightVector.dot(weightVector)
 
-        # add likelihood penalty
-        objective += weightVector.dot(marginals - self.dataSum / len(self.data))
+        # add likelihood penalty log Z - labelEnergy
+        objective += self.bp.computeEnergyFunctional()
+        objective -= weightVector.dot(self.dataSum / len(self.data))
+
 
         return objective
 
@@ -133,6 +135,9 @@ class ApproxMaxLikelihood(object):
 
 def main():
     """Simple test function for maximum likelihood."""
+
+    np.random.seed(0)
+
     mn = MarkovNet()
 
     mn.setUnaryFactor(0, np.zeros(4))
@@ -144,6 +149,8 @@ def main():
 
     aml = ApproxMaxLikelihood(mn)
 
+    aml.setRegularization(0, 1)
+
     aml.addData({0:0, 1:0, 2:0})
     aml.addData({0:1, 1:1, 2:1})
     aml.addData({0:2, 1:2, 2:1})
@@ -151,6 +158,9 @@ def main():
     # print aml.data
 
     weights = np.random.randn(4 + 3 + 2 + 4*3 + 3*2)
+
+    print "\n\nGradient check:"
+    print check_grad(aml.objective, aml.gradient, weights)
 
     print aml.objective(weights)
     print aml.gradient(weights)

BeliefPropagator.py
@@ -139,10 +139,10 @@ class BeliefPropagator(object):
 
         for var in self.mn.variables:
             neighbors = self.mn.getNeighbors(var)
-            energy += np.sum(self.mn.unaryPotentials[var] * self.varBeliefs[var])
+            energy += self.mn.unaryPotentials[var].dot(np.exp(self.varBeliefs[var]))
             for neighbor in neighbors:
                 if var < neighbor:
-                    energy += np.sum(self.mn.getPotential((var, neighbor)) * self.pairBeliefs[(var, neighbor)])
+                    energy += np.sum(self.mn.getPotential((var, neighbor)) * np.exp(self.pairBeliefs[(var, neighbor)]))
         return energy
 
     def computeEnergyFunctional(self):
@@ -158,7 +158,7 @@ class BeliefPropagator(object):
             unaryBelief = np.exp(self.varBeliefs[var])
             for neighbor in self.mn.getNeighbors(var):
                 pairBelief = np.sum(np.exp(self.pairBeliefs[(var, neighbor)]), 1)
-                objective += self.messages[(neighbor, var)].dot(pairBelief - unaryBelief)
+                objective += self.messages[(neighbor, var)].dot(unaryBelief - pairBelief)
         return objective
 
 def main():

MarkovNet.py
@@ -16,9 +16,15 @@ class MarkovNet(object):
         """Set the potential function for the unary factor. Implicitly declare variable. Must be called before setting edge factors."""
         self.unaryPotentials[variable] = potential
         if variable not in self.variables:
+            self.declareVariable(variable, np.size(potential))
+
+    def declareVariable(self, variable, numStates):
+        if variable not in self.variables:
             self.variables.add(variable)
             self.neighbors[variable] = set()
-            self.numStates[variable] = np.size(potential)
+            self.numStates[variable] = numStates
+        else:
+            print("Warning: declaring a variable %s that was previously declared." % repr(variable))
 
     def setEdgeFactor(self, edge, potential):
         """Set a factor by inputting the involved variables then the potential function. The potential function should be a np matrix."""