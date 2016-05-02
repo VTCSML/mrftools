66332ebd6b15450c9edd620d224b9431da60b45c
Elaheh Raisi <elahehraisi@Elahehs-MacBook-Pro.local>
2016-05-02 12:22:20 -0400

    add new function for additinal quadratic features

--- functions.py
+++ functions.py
@@ -23,6 +23,8 @@ from TemplatedLogLinearMLE import TemplatedLogLinearMLE
 import PIL
 from PIL import Image
 import time
+import colorsys
+
 
 
 
@@ -89,6 +91,22 @@ def Load_Resize_Image(image,height,width):
 # =====================================
 # Load and Resize Label
 # =====================================
+
+def get_augmented_pixels(pixel,augmented):
+    if augmented == 1:
+        px = np.true_divide(np.array(pixel),255)
+        px = np.concatenate((px,[1]))
+        iu1 = np.triu_indices(4)
+        p1 = np.outer(px,px)
+        pxl = p1[iu1]
+
+#         pxl = p1[iu1][:-1]
+        
+    else:
+        pxl = np.true_divide(np.array(pixel),255)
+#         pxl = np.array(pixel)
+    return pxl
+
 def Load_Resize_Label(label,height,width):
 
 
@@ -144,64 +162,64 @@ def Create_LogLinearModel(height,width,d,numStates):
     model.setAllUnaryFactors()
 #    print ('unary factors are done')
 
-    ########### Set Edge Factor
+#     ########### Set Edge Factor
     #
     left_ind = num_pixels - width + 1
-
+      
     left_pixels = [i for i in range(1,num_pixels+1) if (i % width) == 1 and i != 1 and i != left_ind]
     right_pixels = [i for i in range(1,num_pixels+1) if (i % width) == 0 and i != width and i != num_pixels]
     up_pixels = range(2,width)
     down_pixels = range(left_ind+1 , num_pixels)
     usual_pixels = [i for i in range(1,num_pixels+1) if i not in left_pixels and i not in right_pixels and i not in up_pixels and i not in down_pixels and i not in (1,width,left_ind,num_pixels)]
-
-
+      
+      
     up_pixels = set(up_pixels)
     down_pixels = set(down_pixels)
     usual_pixels = set(usual_pixels)
     left_pixels = set(left_pixels)
     right_pixels = set(right_pixels)
-
-
+     
+     
     all_edges = set()
-
+        
     model.setEdgeFactor((1,2),np.eye(numStates))
     all_edges.add((1,2))
     model.setEdgeFactor((1,1+width),np.eye(numStates))
     all_edges.add((1,1+width))
-
-
+        
+        
     model.setEdgeFactor((width,width-1),np.eye(numStates))
     all_edges.add((width,width-1))
     model.setEdgeFactor((width,width+width),np.eye(numStates))
     all_edges.add((width,width+width))
-
-
+        
+        
     model.setEdgeFactor((left_ind,left_ind +1),np.eye(numStates))
     all_edges.add((left_ind,left_ind +1))
     model.setEdgeFactor((left_ind,left_ind - width),np.eye(numStates))
     all_edges.add((left_ind,left_ind - width))
-
-
+        
+        
     model.setEdgeFactor((num_pixels,num_pixels - 1),np.eye(numStates))
     all_edges.add((num_pixels,num_pixels - 1))
     model.setEdgeFactor((num_pixels,num_pixels - width),np.eye(numStates))
     all_edges.add((num_pixels,num_pixels - width))
-
-
+        
+        
     for i in (left_pixels):
         if (i,i+1) not in all_edges and (i+1,i) not in all_edges :
             model.setEdgeFactor((i,i+1),np.eye(numStates))
             all_edges.add((i,i+1))
-        
+                
         if (i,i-width) not in all_edges and (i-width,i) not in all_edges:
             model.setEdgeFactor((i,i-width),np.eye(numStates))
             all_edges.add((i,i-width))
-        
+                
         if (i,i+width) not in all_edges and (i+width,i) not in all_edges:
             model.setEdgeFactor((i,i+width),np.eye(numStates))
             all_edges.add((i,i+width))
-
-
+        
+        
     for i in (right_pixels):
         if (i,i-1) not in all_edges and (i-1,i) not in all_edges:
             model.setEdgeFactor((i,i-1),np.eye(numStates))
@@ -212,46 +230,46 @@ def Create_LogLinearModel(height,width,d,numStates):
         if (i,i+width) not in all_edges and (i+width,i) not in all_edges:
             model.setEdgeFactor((i,i+width),np.eye(numStates))
             all_edges.add((i,i+width))
-
-
+        
+        
     for i in  up_pixels:
         if (i,i+1) not in all_edges and (i+1,i) not in all_edges:
             model.setEdgeFactor((i,i+1),np.eye(numStates))
             all_edges.add((i,i+1))
-        
+                
         if (i,i-1) not in all_edges and (i-1,i) not in all_edges:
             model.setEdgeFactor((i,i-1),np.eye(numStates))
             all_edges.add((i,i-1))
-        
+                
         if (i,i+width) not in all_edges and (i+width,i) not in all_edges:
             model.setEdgeFactor((i,i+width),np.eye(numStates))
             all_edges.add((i,i+width))
-
-
-
+        
+        
+        
     for i in  down_pixels:
         if (i,i+1) not in all_edges and (i+1,i) not in all_edges:
             model.setEdgeFactor((i,i+1),np.eye(numStates))
             all_edges.add((i,i+1))
-        
+                
         if (i,i-1) not in all_edges and (i-1,i) not in all_edges:
             model.setEdgeFactor((i,i-1),np.eye(numStates))
             all_edges.add((i,i-1))
-        
+                
         if (i,i-width) not in all_edges and (i-width,i) not in all_edges:
             model.setEdgeFactor((i,i-width),np.eye(numStates))
             all_edges.add((i,i-width))
-
-
+        
+        
     for i in (usual_pixels):
         if (i,i+1) not in all_edges and (i+1,i) not in all_edges:
             model.setEdgeFactor((i,i+1),np.eye(numStates))
             all_edges.add((i,i+1))
-        
+                
         if (i,i-1) not in all_edges and (i-1,i) not in all_edges:
             model.setEdgeFactor((i,i-1),np.eye(numStates))
             all_edges.add((i,i-1))
-        
+                
         if (i,i-width) not in all_edges and (i-width,i) not in all_edges:
             model.setEdgeFactor((i,i-width),np.eye(numStates))
             all_edges.add((i,i-width))
@@ -262,9 +280,6 @@ def Create_LogLinearModel(height,width,d,numStates):
 
 #    print ('edge factors are done')
     return model
-
-
-
 
 # =====================================
 # Create Markov Model
@@ -278,68 +293,71 @@ def Create_MarkovNet(height,width,w_unary,w_pair,pixels):
     k = 1
     for i in range(0,height):
         for j in range(0,width):
-            mn.setUnaryFactor(k,np.dot(w_unary,pixels[j,i]))
+            pxl = get_augmented_pixels(pixels[j,i],1)
+
+#             pxl = np.array(pixels[j,i])
+            mn.setUnaryFactor(k,np.dot(w_unary,pxl))
 
             k += 1
 
 #    print ('setUnaryFactor done------')
-    ##########Set Pairwise
-
+#     ##########Set Pairwise
+# 
     num_pixels = height * width
     left_ind = num_pixels - width + 1
-
+        
     left_pixels = [i for i in range(1,num_pixels+1) if (i % width) == 1 and i != 1 and i != left_ind]
     right_pixels = [i for i in range(1,num_pixels+1) if (i % width) == 0 and i != width and i != num_pixels]
     up_pixels = range(2,width)
     down_pixels = range(left_ind+1 , num_pixels)
     usual_pixels = [i for i in range(1,num_pixels+1) if i not in left_pixels and i not in right_pixels and i not in up_pixels and i not in down_pixels and i not in (1,width,left_ind,num_pixels)]
-
-
-    up_pixels = set(up_pixels)
+        
+        
+#     up_pixels = set(up_pixels)
     down_pixels = set(down_pixels)
     usual_pixels = set(usual_pixels)
     left_pixels = set(left_pixels)
     right_pixels = set(right_pixels)
-
+        
     all_edges = set()
-
-
-
+        
+        
+        
     mn.setEdgeFactor((1,2), w_pair)
     all_edges.add((1,2))
     mn.setEdgeFactor((1,1+width),w_pair)
     all_edges.add((1,1+width))
-
+        
     mn.setEdgeFactor((width,width-1),w_pair)
     all_edges.add((width,width-1))
     mn.setEdgeFactor((width,width+width),w_pair)
     all_edges.add((width,width+width))
-
-
+        
+        
     mn.setEdgeFactor((left_ind,left_ind +1),w_pair)
     all_edges.add((left_ind,left_ind +1))
     mn.setEdgeFactor((left_ind,left_ind - width),w_pair)
     all_edges.add((left_ind,left_ind - width))
-
+        
     mn.setEdgeFactor((num_pixels,num_pixels - 1),w_pair)
     all_edges.add((num_pixels,num_pixels - 1))
     mn.setEdgeFactor((num_pixels,num_pixels - width),w_pair)
     all_edges.add((num_pixels,num_pixels - width))
-
-
+        
+        
     for i in (left_pixels):
         if (i,i+1) not in all_edges and (i+1,i) not in all_edges :
             mn.setEdgeFactor((i,i+1),w_pair)
             all_edges.add((i,i+1))
-        
+                
         if (i,i-width) not in all_edges and (i-width,i) not in all_edges:
             mn.setEdgeFactor((i,i-width),w_pair)
             all_edges.add((i,i-width))
-        
+                
         if (i,i+width) not in all_edges and (i+width,i) not in all_edges:
             mn.setEdgeFactor((i,i+width),w_pair)
             all_edges.add((i,i+width))
-
+        
     for i in (right_pixels):
         if (i,i-1) not in all_edges and (i-1,i) not in all_edges:
             mn.setEdgeFactor((i,i-1),w_pair)
@@ -350,42 +368,42 @@ def Create_MarkovNet(height,width,w_unary,w_pair,pixels):
         if (i,i+width) not in all_edges and (i+width,i) not in all_edges:
             mn.setEdgeFactor((i,i+width),w_pair)
             all_edges.add((i,i+width))
-
+        
     for i in  up_pixels:
         if (i,i+1) not in all_edges and (i+1,i) not in all_edges:
             mn.setEdgeFactor((i,i+1),w_pair)
             all_edges.add((i,i+1))
-        
+                
         if (i,i-1) not in all_edges and (i-1,i) not in all_edges:
             mn.setEdgeFactor((i,i-1),w_pair)
             all_edges.add((i,i-1))
-        
+                
         if (i,i+width) not in all_edges and (i+width,i) not in all_edges:
             mn.setEdgeFactor((i,i+width),w_pair)
             all_edges.add((i,i+width))
-
+        
     for i in  down_pixels:
         if (i,i+1) not in all_edges and (i+1,i) not in all_edges:
             mn.setEdgeFactor((i,i+1),w_pair)
             all_edges.add((i,i+1))
-        
+                
         if (i,i-1) not in all_edges and (i-1,i) not in all_edges:
             mn.setEdgeFactor((i,i-1),w_pair)
             all_edges.add((i,i-1))
-        
+                
         if (i,i-width) not in all_edges and (i-width,i) not in all_edges:
             mn.setEdgeFactor((i,i-width),w_pair)
             all_edges.add((i,i-width))
-
+        
     for i in (usual_pixels):
         if (i,i+1) not in all_edges and (i+1,i) not in all_edges:
             mn.setEdgeFactor((i,i+1),w_pair)
             all_edges.add((i,i+1))
-        
+                
         if (i,i-1) not in all_edges and (i-1,i) not in all_edges:
             mn.setEdgeFactor((i,i-1),w_pair)
             all_edges.add((i,i-1))
-        
+                
         if (i,i-width) not in all_edges and (i-width,i) not in all_edges:
             mn.setEdgeFactor((i,i-width),w_pair)
             all_edges.add((i,i-width))
