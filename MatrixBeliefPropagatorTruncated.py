c0fb7471c65843830c0f354b89045766370d36e5
Shuangfei Fan <sophia@Shuangfeis-MacBook-Pro.local>
2016-05-20 16:04:12 -0400

    matrix version of belief propagator and loglinearMLE

---
+++ MatrixBeliefPropagatorTruncated.py
@@ -0,0 +1,301 @@
+"""BeliefPropagator class."""
+import autograd.numpy as np
+from MarkovNet import MarkovNet
+from autograd.core import primitive
+
+class MatrixBeliefPropagatorTruncated(object):
+    """Object that can run belief propagation on a MarkovNet."""
+
+    def __init__(self, markovNet):
+        """Initialize belief propagator for markovNet."""
+        self.mn = markovNet
+        self.varBeliefs = dict()
+        self.pairBeliefs = dict()
+
+        if not self.mn.matrix_mode:
+            self.mn.create_matrices()
+
+        self.previously_initialized = False
+        self.initialize_messages()
+
+        self.belief_mat = np.zeros((self.mn.max_states, len(self.mn.variables)))
+        self.pair_belief_tensor = np.zeros((self.mn.max_states, self.mn.max_states, self.mn.num_edges))
+
+    def initialize_messages(self):
+        self.message_mat = np.zeros((self.mn.max_states, 2 * self.mn.num_edges))
+
+
+    def computeBeliefs(self):
+        """Compute unary beliefs based on current messages."""
+        self.belief_mat = self.mn.unary_mat + self.mn.message_to_index.T.dot(self.message_mat.T).T
+        logZ = logsumexp(self.belief_mat, 0)
+
+        self.belief_mat = self.belief_mat - logZ
+
+
+    def computePairwiseBeliefs(self):
+        """Compute pairwise beliefs based on current messages."""
+
+        adjusted_message_prod = self.mn.message_from_index.dot(self.belief_mat.T).T \
+                                - np.hstack((self.message_mat[:, self.mn.num_edges:],
+                                                           self.message_mat[:, :self.mn.num_edges]))
+
+        to_messages = adjusted_message_prod[:, :self.mn.num_edges].reshape((self.mn.max_states, 1, self.mn.num_edges))
+        from_messages = adjusted_message_prod[:, self.mn.num_edges:].reshape((1, self.mn.max_states, self.mn.num_edges))
+
+        beliefs = self.mn.edge_pot_tensor[:, :, self.mn.num_edges:] + to_messages + from_messages
+
+        max_val = beliefs.max(axis=(0, 1), keepdims=True)
+
+        log_partitions = np.log(np.sum(np.exp(beliefs - max_val), axis = (0, 1), keepdims=True)) + max_val
+
+        beliefs -= log_partitions
+
+        self.pair_belief_tensor = beliefs
+
+
+    def updateMessages(self):
+        """Update all messages between variables using belief division. Return the change in messages from previous iteration."""
+        self.computeBeliefs()
+
+        adjusted_message_prod = self.mn.message_from_index.dot(self.belief_mat.T).T \
+                                - np.hstack((self.message_mat[:, self.mn.num_edges:],
+                                             self.message_mat[:, :self.mn.num_edges]))
+
+        messages = np.squeeze(logsumexp(np.nan_to_num(self.mn.edge_pot_tensor + adjusted_message_prod), 1))
+        messages -= messages.max(0)
+
+        change = np.sum(np.abs(messages - self.message_mat))
+
+        self.message_mat = np.nan_to_num(messages)
+
+        return change
+
+    def _compute_inconsistency_vector(self):
+
+        expanded_beliefs = np.exp(self.mn.message_to_index.dot(self.belief_mat.T).T)
+
+        pairwise_beliefs = np.hstack((np.sum(np.exp(self.pair_belief_tensor), axis = 0),
+                                      np.sum(np.exp(self.pair_belief_tensor), axis = 1)))
+
+        return expanded_beliefs - pairwise_beliefs
+
+
+    def computeInconsistency(self):
+        """Return the total disagreement between each unary belief and its pairwise beliefs."""
+        disagreement = np.sum(np.abs(self._compute_inconsistency_vector()))
+
+        return disagreement
+
+    def runInference(self, tolerance = 1e-8, display = 'iter', maxIter = 300):
+        """Run belief propagation until messages change less than tolerance."""
+        change = np.inf
+        iteration = 0
+        while change > tolerance and iteration < maxIter:
+            change = self.updateMessages()
+            if display == "full":
+                disagreement = self.computeInconsistency()
+                energyFunc = self.computeEnergyFunctional()
+                dualObj = self.computeDualObjective()
+                print("Iteration %d, change in messages %f. Calibration disagreement: %f, energy functional: %f, dual obj: %f" % (iteration, change, disagreement, energyFunc, dualObj))
+            elif display == "iter":
+                print("Iteration %d, change in messages %f." % (iteration, change))
+            iteration += 1
+        if display == 'final' or display == 'full' or display == 'iter':
+            print("Belief propagation finished in %d iterations." % iteration)
+
+    def load_beliefs(self):
+
+        for (var, i) in self.mn.var_index.items():
+            self.varBeliefs[var] = self.belief_mat[:len(self.mn.unaryPotentials[var]), i]
+
+        for i in range(self.mn.num_edges):
+            (var, neighbor) = self.mn.edges[i]
+
+            belief = self.pair_belief_tensor[:len(self.mn.unaryPotentials[var]),
+                     :len(self.mn.unaryPotentials[neighbor]), i]
+
+            self.pairBeliefs[(var, neighbor)] = belief
+
+            self.pairBeliefs[(neighbor, var)] = belief.T
+
+    def computeBetheEntropy(self):
+        """Compute Bethe entropy from current beliefs. Assume that the beliefs have been computed and are fresh."""
+        entropy = - np.sum(np.nan_to_num(self.pair_belief_tensor) * np.exp(self.pair_belief_tensor)) \
+                  - np.sum((1 - self.mn.degrees) * (np.nan_to_num(self.belief_mat) * np.exp(self.belief_mat)))
+
+        return entropy
+
+    def computeEnergy(self):
+        """Compute the log-linear energy. Assume that the beliefs have been computed and are fresh."""
+        energy = np.sum(np.nan_to_num(self.mn.edge_pot_tensor[:, :, self.mn.num_edges:]) * np.exp(self.pair_belief_tensor)) + \
+                 np.sum(np.nan_to_num(self.mn.unary_mat) * np.exp(self.belief_mat))
+
+        return energy
+
+    def computeEnergyFunctional(self):
+        """Compute the energy functional."""
+        self.computeBeliefs()
+        self.computePairwiseBeliefs()
+        return self.computeEnergy() + self.computeBetheEntropy()
+
+    def computeDualObjective(self):
+        """Compute the value of the BP Lagrangian."""
+        objective = self.computeEnergyFunctional() + \
+                    np.sum(self.message_mat * self._compute_inconsistency_vector())
+
+        return objective
+
+
+@primitive
+def logsumexp(matrix, dim = None):
+    """Compute log(sum(exp(matrix), dim)) in a numerically stable way."""
+
+    if matrix.size <= 1:
+        return matrix
+
+    maxVal = matrix.max(dim, keepdims=True)
+    return np.log(np.sum(np.exp(matrix - maxVal), dim, keepdims=True)) + maxVal
+
+def make_grad_logsumexp(ans, matrix, dim = None):
+    def gradient_product(g):
+        return np.full(matrix.shape, g) * np.exp(matrix - np.full(matrix.shape, ans))
+    return gradient_product
+
+logsumexp.defgrad(make_grad_logsumexp)
+
+
+def main():
+    """Test basic functionality of BeliefPropagator."""
+    from BeliefPropagator import BeliefPropagator
+
+    mn = MarkovNet()
+
+    np.random.seed(1)
+
+    np.set_printoptions(precision=5)
+    # np.seterr(all='raise')
+
+    k = [2, 2, 3, 3, 3]
+
+    mn.setUnaryFactor(0, np.random.randn(k[0]))
+    mn.setUnaryFactor(1, np.random.randn(k[1]))
+    mn.setUnaryFactor(2, np.random.randn(k[2]))
+    mn.setUnaryFactor(3, np.random.randn(k[3]))
+    mn.setUnaryFactor(4, np.random.randn(k[3]))
+
+    # factor4 = np.random.randn(k[4])
+    # factor4[2] = -float('inf')
+
+    # mn.setUnaryFactor(4, factor4)
+
+    mn.setEdgeFactor((0,1), np.random.randn(k[0], k[1]))
+    mn.setEdgeFactor((1,2), np.random.randn(k[1], k[2]))
+    mn.setEdgeFactor((3,2), np.random.randn(k[3], k[2]))
+    mn.setEdgeFactor((3,4), np.random.randn(k[3], k[4]))
+    # mn.setEdgeFactor((1,4), np.random.randn(k[1], k[4]))
+    # mn.setEdgeFactor((3,0), np.random.randn(k[3], k[0])) # uncomment this to make loopy
+
+    print("Neighbors of 0: " + repr(mn.getNeighbors(0)))
+    print("Neighbors of 1: " + repr(mn.getNeighbors(1)))
+
+    bp = MatrixBeliefPropagator(mn)
+
+    # for t in range(15):
+    #     change = bp.updateMessages()
+    #     disagreement = bp.computeInconsistency()
+    #     print("Iteration %d, change in messages %f. Calibration disagreement: %f" % (t, change, disagreement))
+
+    bp.runInference(display='full')
+
+    bp.computePairwiseBeliefs()
+
+    bp.load_beliefs()
+
+    from BruteForce import BruteForce
+
+    bf = BruteForce(mn)
+
+    unary_error = 0
+
+    for i in mn.variables:
+        bf_marg = bf.unaryMarginal(i)
+        bp_marg = np.exp(bp.varBeliefs[i])
+
+        unary_error += np.sum(np.abs(bf_marg - bp_marg))
+
+        print ("Brute force unary marginal of %d: %s" % (i, repr(bf_marg)))
+        print ("Belief prop unary marginal of %d: %s" % (i, repr(bp_marg)))
+
+    pairwise_error = 0.0
+
+    for var in mn.variables:
+        for neighbor in mn.getNeighbors(var):
+            edge = (var, neighbor)
+            bf_marg = bf.pairwiseMarginal(var, neighbor)
+            bp_marg = np.exp(bp.pairBeliefs[edge])
+
+            pairwise_error += np.sum(np.abs(bf_marg - bp_marg))
+
+            print ("Brute force pairwise marginal of %s: %s" % (repr(edge), repr(bf_marg)))
+            print ("Belief prop pairwise marginal of %s: %s" % (repr(edge), repr(bp_marg)))
+
+    print ("Unary error %s, pairwise error %s" % (unary_error, pairwise_error))
+
+    print ("Bethe energy functional: %f" % bp.computeEnergyFunctional())
+
+    print ("Brute force log partition function: %f" % np.log(bf.computeZ()))
+
+
+    print ("Brute force entropy: %f" % np.log(bf.entropy()))
+    print ("Bethe entropy: %f" % bp.computeBetheEntropy())
+    print ("Belief prop energy: %f" % bp.computeEnergy())
+
+    bp_old = BeliefPropagator(mn)
+
+    bp_old.runInference(display='full')
+
+    print ("Old Bethe energy functional: %f" % bp_old.computeEnergyFunctional())
+    print ("Old Bethe entropy: %f" % bp_old.computeBetheEntropy())
+    print ("Old belief prop energy: %f" % bp_old.computeEnergy())
+
+    print("Running grid timing comparison to loop BP")
+
+    mn = MarkovNet()
+
+    length = 32
+
+    k = 8
+
+    for x in range(length):
+        for y in range(length):
+            mn.setUnaryFactor((x, y), np.random.random(k))
+
+    for x in range(length - 1):
+        for y in range(length):
+            mn.setEdgeFactor(((x, y), (x + 1, y)), np.random.random((k, k)))
+            mn.setEdgeFactor(((y, x), (y, x + 1)), np.random.random((k, k)))
+
+    log_bp = BeliefPropagator(mn)
+
+    bp = MatrixBeliefPropagator(mn)
+
+    import time
+
+    t0 = time.time()
+    bp.runInference(display='final', maxIter=30000)
+    t1 = time.time()
+
+    bp_time = t1 - t0
+
+    t0 = time.time()
+    log_bp.runInference(display='final', maxIter=30000)
+    t1 = time.time()
+
+    log_bp_time = t1 - t0
+
+    print("Matrix BP took %f, loop-based BP took %f. Speedup was %f" % (bp_time, log_bp_time, log_bp_time / bp_time))
+
+
+if  __name__ =='__main__':
+    main()
---
+++ MatrixLogLinearMLETruncated.py
@@ -0,0 +1,235 @@
+from LogLinearModel import LogLinearModel
+import autograd.numpy as np
+import copy
+from scipy.optimize import minimize, check_grad, approx_fprime
+from MatrixBeliefPropagatorTruncated import MatrixBeliefPropagatorTruncated
+from MatrixCache import MatrixCache
+
+class MatrixLogLinearMLETruncated(object):
+    """Object that runs approximate maximum likelihood parameter training."""
+
+    def __init__(self, baseModel, bpIters):
+        """Initialize learner. Pass in Markov net."""
+        assert isinstance(baseModel, LogLinearModel)
+        self.baseModel = baseModel
+        self.models = []
+        self.beliefPropagators = []
+        self.labels = []
+        self.l1Regularization = 0.00
+        self.l2Regularization = 1
+        self.featureSum = 0
+        self.prevWeights = 0
+        self.bpIters = bpIters
+        self.baseModel.create_matrices()
+
+    def setRegularization(self, l1, l2):
+        """Set the regularization parameters."""
+        self.l1Regularization = l1
+        self.l2Regularization = l2
+
+    def addData(self, states, features):
+        """Add data example to training set. The states variable should be a dictionary containing all the states of the unary variables. Features should be a dictionary containing the feature vectors for the unary variables."""
+        example = []
+
+        model = copy.deepcopy(self.baseModel)
+
+        labeled_feature_mat = np.zeros((model.max_states, model.max_features))
+        feature_mat = np.zeros((model.max_features, len(model.variables)))
+
+        for (var, i) in model.var_index.items():
+            labeled_feature_mat[states[var], :] += features[var]
+            feature_mat[:, i] = features[var]
+
+        model.feature_mat = feature_mat
+
+        pairwise = np.zeros((model.max_states, model.max_states))
+        for k in range(model.num_edges):
+            (var, neighbor) = model.edges[k]
+            #pairwise[states[model.var_list[i-1]], states[model.var_list[j-1]]] += 1
+            pairwise[states[var], states[neighbor]] += 1
+
+        example = np.append(labeled_feature_mat.flatten(), pairwise.flatten())
+
+        self.models.append(model)
+        self.beliefPropagators.append(MatrixBeliefPropagatorTruncated(model))
+        self.labels.append(example)
+        self.featureSum += example
+
+    def setWeights(self, weight_vector, models):
+        """Set weights of Markov net from vector using the order in self.potentials."""
+
+        max_features = self.baseModel.max_features
+        num_vars = len(self.baseModel.variables)
+        max_states = self.baseModel.max_states
+        num_edges = self.baseModel.num_edges
+
+        feature_size = max_features * max_states
+
+        feature_weights = weight_vector[:feature_size].reshape((max_features, max_states))
+
+        pairwise_weights = weight_vector[feature_size:].reshape((max_states, max_states, 1)) * np.ones((1, 1, num_edges))
+
+        for model in models:
+            model.set_weight_matrix(feature_weights)
+            model.set_edge_tensor(pairwise_weights)
+
+            model.set_unary_matrix()
+
+
+    def getFeatureExpectations(self, beliefPropagators):
+        """Run inference and return the marginal in vector form using the order of self.potentials.
+        """
+        marginalSum = 0
+        for i in range(len(self.labels)):
+            bp = beliefPropagators[i]
+            model = bp.mn
+            bp.initialize_messages()
+            bp.runInference(display = 'off', maxIter = self.bpIters)
+            bp.computeBeliefs()
+            bp.computePairwiseBeliefs()
+
+            summed_features = np.inner(np.exp(bp.belief_mat), model.feature_mat).T
+
+            summed_pair_features = np.sum(np.exp(bp.pair_belief_tensor), 2).T
+
+            marginals = np.append(summed_features.reshape(-1), summed_pair_features.reshape(-1))
+
+            marginalSum += marginals
+
+        return marginalSum / len(self.labels)
+
+    def objective(self, weightVector):
+        """Compute the learning objective with the provided weight vector."""
+        self.setWeights(weightVector, self.models)
+        featureExpectations = self.getFeatureExpectations(self.beliefPropagators)
+
+        objective = 0.0
+
+        # add regularization penalties
+        objective += self.l1Regularization * np.sum(np.abs(weightVector))
+        objective += 0.5 * self.l2Regularization * np.dot(weightVector, weightVector)
+
+        # add likelihood penalty
+        objective -= np.dot(weightVector, self.featureSum / len(self.labels))
+
+        for bp in self.beliefPropagators:
+            objective += bp.computeEnergyFunctional() / len(self.labels)
+            # print "Finished one inference"
+
+        return objective
+
+    def dualObjective(self, weightVector):
+        """Compute the learning objective with the provided weight vector."""
+        self.setWeights(weightVector, self.models)
+        featureExpectations = self.getFeatureExpectations(self.beliefPropagators)
+
+        dObjective = 0.0
+
+        # add regularization penalties
+        dObjective += self.l1Regularization * np.sum(np.abs(weightVector))
+        dObjective += 0.5 * self.l2Regularization * np.dot(weightVector, weightVector)
+
+        # add likelihood penalty
+        dObjective -= np.dot(weightVector, self.featureSum / len(self.labels))
+
+        for bp in self.beliefPropagators:
+            dObjective += bp.computeDualObjective() / len(self.labels)
+            # print "Finished one inference"
+
+        return dObjective
+
+    def gradient(self, weightVector):
+        """Compute the gradient for the provided weight vector."""
+        self.setWeights(weightVector, self.models)
+        inferredExpectations = self.getFeatureExpectations(self.beliefPropagators)
+
+        gradient = np.zeros(len(weightVector))
+
+        # add regularization penalties
+        gradient += self.l1Regularization * np.sign(weightVector)
+        gradient += self.l2Regularization * weightVector
+
+        # add likelihood penalty
+        gradient += inferredExpectations - self.featureSum / len(self.labels)
+
+        return gradient
+
+def main():
+    """Simple test function for maximum likelihood."""
+
+    np.set_printoptions(precision=3)
+
+    model = LogLinearModel()
+
+    np.random.seed(1)
+
+    model.declareVariable(0, 4)
+    model.declareVariable(1, 4)
+    model.declareVariable(2, 4)
+
+    d = 2
+
+    model.setUnaryWeights(0, np.random.randn(4, d))
+    model.setUnaryWeights(1, np.random.randn(4, d))
+    model.setUnaryWeights(2, np.random.randn(4, d))
+
+    model.setUnaryFeatures(0, np.random.randn(d))
+    model.setUnaryFeatures(1, np.random.randn(d))
+    model.setUnaryFeatures(2, np.random.randn(d))
+
+    model.setAllUnaryFactors()
+
+    model.setEdgeFactor((0,1), np.zeros((4, 4)))
+    model.setEdgeFactor((1,2), np.zeros((4, 4)))
+
+    from TemplatedLogLinearMLE import TemplatedLogLinearMLE
+
+    learner = MatrixLogLinearMLE(model)
+
+    data = [({0: 0, 1: 0, 2: 0}, {0: np.random.randn(d), 1: np.random.randn(d), 2: np.random.randn(d)}),
+            ({0: 1, 1: 1, 2: 0}, {0: np.random.randn(d), 1: np.random.randn(d), 2: np.random.randn(d)}),
+            ({0: 2, 1: 0, 2: 1}, {0: np.random.randn(d), 1: np.random.randn(d), 2: np.random.randn(d)}),
+            ({0: 3, 1: 2, 2: 0}, {0: np.random.randn(d), 1: np.random.randn(d), 2: np.random.randn(d)})]
+
+    # add unary weights
+    weights = np.ones(4 * d)
+    # add edge weights
+    weights = np.append(weights, np.ones(4 * 4))
+
+    import time
+
+    print(learner)
+
+    for (states, features) in data:
+        learner.addData(states, features)
+
+    learner.setRegularization(.2, 1)
+
+    print "\n\nObjective:"
+    print learner.objective(weights)
+    print "\n\nGradient:"
+    print learner.gradient(weights)
+
+
+    print "\n\nGradient check:"
+    print check_grad(learner.objective, learner.gradient, weights)
+
+    t0 = time.time()
+
+    print "\n\nOptimization:"
+
+
+    res = minimize(learner.objective, weights, method='L-BFGS-b', jac = learner.gradient)
+
+    t1 = time.time()
+
+    print("Optimization took %f seconds" % (t1 - t0))
+
+    print res
+
+    print "\n\nGradient check at optimized solution:"
+    print check_grad(learner.objective, learner.gradient, res.x * (1 + 1e-9))
+
+
+if  __name__ =='__main__':
+    main()
