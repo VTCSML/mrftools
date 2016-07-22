0da7f09533150ddd537ef550c49b67ac0e4fcc72
Sirui Yao <ysirui@vt.edu>
2016-07-22 14:51:53 -0400

    Added unary classification test for computed features

--- tests/test_imageloader.py
+++ tests/test_imageloader.py
@@ -5,6 +5,7 @@ from PIL import Image
 import PIL
 import os
 from ImageLoader import ImageLoader
+from scipy.optimize import minimize, check_grad
 
 
 class TestImageLoader(unittest.TestCase):
@@ -21,7 +22,100 @@ class TestImageLoader(unittest.TestCase):
             assert np.allclose(len(labels[i]), img.width * img.height), "the size of labels is right"
             assert np.allclose(len(features), img.width * img.height), "the size of features is right"
 
+    def test_unary_only(self):
+        num_features = 65
+        num_states = 2
+
+        all_pixel, all_label = load_all_images_and_labels('./train', num_features)
+
+        initial_w = np.random.rand(num_features * num_states, 1)
+        res = minimize(objective, initial_w, method="L-BFGS-B", args=(all_pixel, all_label, num_features, num_states),
+                       jac=gradient)
+        weights = res.x
+
+        accuracy_training = accuracy(weights, all_pixel, all_label, num_features, num_states)
+        print ("accuracy on training set: %f" % (accuracy_training))
+        assert (accuracy_training >= 0.9), "Unary classification accuracy on training data is less than 0.9"
+
+        all_pixel, all_label = load_all_images_and_labels('./test', num_features)
+        accuracy_testing = accuracy(weights, all_pixel, all_label, num_features, num_states)
+        print ("accuracy on testing set: %f" % (accuracy_testing))
+        assert (accuracy_testing >= 0.8), "Unary classification accuracy on testing data is less than 0.9"
+
+
+
+def softmax(x):
+    x_shifted = x - np.max(x, axis=1, keepdims=True)
+    return np.exp(x_shifted) / np.sum(np.exp(x_shifted), 1, keepdims=True)
+
+
+def objective(weights, features, label_vec, num_features, num_states):
+    probabilities = np.dot(features, weights.reshape((num_features, num_states)))
+    probabilities = softmax(probabilities)
+
+    label_mat = np.zeros((probabilities.shape))
+
+    for i in range(num_states):
+        label_mat[:, i] = np.where(label_vec == i, 1, 0)
+
+    return -np.sum(label_mat * np.nan_to_num(np.log(probabilities))) + np.dot(weights.ravel(), weights.ravel())
+    # return -np.sum(label_mat * np.nan_to_num(np.log(probabilities)))
+
+
+def gradient(weights, features, label_vec, num_features, num_states):
+    probabilities = np.dot(features, weights.reshape((num_features, num_states)))
+    probabilities = softmax(probabilities)
+
+    label_mat = np.zeros((probabilities.shape))
+
+    for i in range(num_states):
+        label_mat[:, i] = np.where(label_vec == i, 1, 0)
+
+    g = -features.T.dot(label_mat - probabilities).ravel() + 2 * weights.ravel()
+    # g = -features.T.dot(label_mat - probabilities).ravel()
+    return g
+
+
+def accuracy(weights, features, label_vec,  num_features, num_states):
+    total_error = 0
+    probabilities = np.dot(features, weights.reshape((num_features, num_states)))
+    probabilities = softmax(probabilities)
+    positive_vec = np.argmax(probabilities, axis=1)
+    error = np.sum(np.abs(positive_vec - label_vec))
+    num_pixels = np.shape(label_vec)
+
+    total_error = total_error + error
+    accuracy = 1 - (total_error / num_pixels)
+
+    return accuracy
+
+
+####################
+def load_all_images_and_labels(path, num_features):
+    loader = ImageLoader()
+    all_pixel = np.random.randn(0,num_features)
+    all_label = []
+
+# files = [x for x in os.listdir('./train') if x.endswith(".jpg") or x.endswith('.png')]
+    files = [x for x in os.listdir(path) if x.endswith(".jpg") or x.endswith('.png')]
+    for i, filename in enumerate(files):
+        full_name = os.path.join(path, filename)
+        # full_name = os.path.join('./train', filename)
+        img = Image.open(full_name)
+        height = img.size[1]
+        width = img.size[0]
+
+        features = ImageLoader.compute_features(img)
+        pixel = np.asarray(list(features.values()))
+        all_pixel = np.concatenate((all_pixel, pixel), axis=0)
+
+
+        label_dict = loader.load_label_dict(full_name)
+        label_vec = np.asarray(list(label_dict.values()))
+        all_label = np.concatenate((all_label, label_vec), axis=0)
+
+    return all_pixel, all_label
+
 
 if __name__ == '__main__':
     unittest.main()
-
