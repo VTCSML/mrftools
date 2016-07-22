fixed gradient and objective bugs introduced from edge features. Added gradient tests for EM and subgradient learning

Learner.py
@@ -31,7 +31,8 @@ class Learner(object):
         self.l2_regularization = l2
         
     def add_data(self, labels, model):
-        """Add data example to training set. The states variable should be a dictionary containing all the states of the unary variables. Features should be a dictionary containing the feature vectors for the unary variables."""
+        """Add data example to training set. The states variable should be a dictionary containing all the states of the
+         unary variables. Features should be a dictionary containing the feature vectors for the unary variables."""
 
         self.models.append(model)
         self.belief_propagators.append(self.inference_type(model))
@@ -85,10 +86,8 @@ class Learner(object):
 
         while not np.allclose(old_weights, new_weights):
             old_weights = new_weights
-            res = minimize(self.subgrad_obj, new_weights, args=['subgradient'], method='L-BFGS-B', jac=self.subgrad_grad,
-                           callback=self.callback_f)
+            res = minimize(self.subgrad_obj, new_weights, method='L-BFGS-B', jac=self.subgrad_grad, callback=self.callback_f)
             new_weights = res.x
-            # print check_grad(self.subgrad_obj, self.subgrad_grad, new_weights, 'subgradient')
 
         return new_weights
 
@@ -107,13 +106,13 @@ class Learner(object):
             self.weight_record = np.vstack((self.weight_record,a))
             self.time_record = np.vstack((self.time_record,int(round(time.time() * 1000))))
 
-    def set_weights(self, weight_vector, models):
+    def set_weights(self, weight_vector, belief_propagators):
         """Set weights of Markov net from vector using the order in self.potentials."""
-        for model in models:
-            model.set_weights(weight_vector)
+        for bp in belief_propagators:
+            bp.mn.set_weights(weight_vector)
 
     def calculate_tau(self, weights, belief_propagators, should_infer=True):
-        self.set_weights(weights, self.models_q)
+        self.set_weights(weights, belief_propagators)
         if should_infer:
             self.do_inference(belief_propagators)
 
@@ -121,6 +120,7 @@ class Learner(object):
 
     def objective(self, weights, options=None):
         self.tau_p = self.calculate_tau(weights, self.belief_propagators, True)
+        self.set_weights(weights, self.belief_propagators_q)
 
         term_p = sum([x.compute_energy_functional() for x in self.belief_propagators]) / self.num_examples
         term_q = sum([x.compute_energy_functional() for x in self.belief_propagators_q]) / self.num_examples
@@ -135,7 +135,8 @@ class Learner(object):
         return objec
         
     def gradient(self, weights, options=None):
-        self.calculate_tau(weights, self.belief_propagators, False)
+        self.tau_p = self.calculate_tau(weights, self.belief_propagators, False)
+        self.set_weights(weights, self.belief_propagators_q)
 
         grad = np.zeros(len(weights))
       

MatrixBeliefPropagator.py
@@ -150,7 +150,7 @@ class MatrixBeliefPropagator(Inference):
 
         summed_features = np.inner(np.exp(self.belief_mat), self.mn.feature_mat).T
 
-        summed_pair_features = np.sum(np.exp(self.pair_belief_tensor), 2).T
+        summed_pair_features = np.sum(np.exp(self.pair_belief_tensor) * self.mn.edge_feature_mat, 2)
 
         marginals = np.append(summed_features.reshape(-1), summed_pair_features.reshape(-1))
 

tests/test_integration.py
@@ -9,11 +9,13 @@ import matplotlib.pyplot as plt
 class IntegrationTest(unittest.TestCase):
 
     def test_loading_and_learning(self):
-        loader = ImageLoader(32, 32)
+        loader = ImageLoader(16, 16)
 
         images, models, labels, names = loader.load_all_images_and_labels('./train', 2)
 
         learner = Learner(MatrixBeliefPropagator)
+
+        learner.set_regularization(0.0, 0.00001)
 
         for model, states in zip(models, labels):
             learner.add_data(states, model)
@@ -33,23 +35,27 @@ class IntegrationTest(unittest.TestCase):
 
         models[i].set_weights(new_weights)
         bp = MatrixBeliefPropagator(models[i])
-        bp.infer()
+        bp.infer(display='final')
         bp.load_beliefs()
 
         beliefs = np.zeros((images[i].height, images[i].width))
+        label_img = np.zeros((images[i].height, images[i].width))
         errors = 0
         baseline = 0
 
         for x in range(images[i].width):
             for y in range(images[i].height):
                 beliefs[y, x] = np.exp(bp.var_beliefs[(x, y)][1])
+                label_img[y, x] = labels[i][(x, y)]
                 errors += np.abs(labels[i][(x, y)] - np.round(beliefs[y, x]))
                 baseline += labels[i][(x, y)]
 
         # # uncomment this to plot the beliefs
-        # plt.subplot(121)
+        # plt.subplot(131)
         # plt.imshow(images[i], interpolation="nearest")
-        # plt.subplot(122)
+        # plt.subplot(132)
+        # plt.imshow(label_img, interpolation="nearest")
+        # plt.subplot(133)
         # plt.imshow(beliefs, interpolation="nearest")
         # plt.show()
 

tests/test_learner.py
@@ -5,6 +5,8 @@ import numpy as np
 from LogLinearModel import LogLinearModel
 from EM import EM
 from PairedDual import PairedDual
+from scipy.optimize import check_grad, approx_fprime
+import matplotlib.pyplot as plt
 
 class TestLearner(unittest.TestCase):
 
@@ -14,22 +16,51 @@ class TestLearner(unittest.TestCase):
 
         np.random.seed(0)
 
-        data = [({0: 2, 2: 1}, {0: np.random.randn(d), 1: np.random.randn(d), 2: np.random.randn(d)}),
-                ({1: 2, 2: 0}, {0: np.random.randn(d), 1: np.random.randn(d), 2: np.random.randn(d)})]
+        labels = [{0: 2,       2: 1},
+                  {      1: 2, 2: 0},
+                  {0: 2, 1: 3,     },
+                  {0: 0, 1: 2, 2: 3}]
 
         models = []
-        labels = []
-        for i in range(len(data)):
-            m = self.create_model(num_states, d)
+        for i in range(len(labels)):
+            m = self.create_random_model(num_states, d)
             models.append(m)
-            dic = data[i][0]
-            label_vec = dic
-            labels.append(label_vec)
-            # for keys,values in dic.items():
-            #     print values
 
         for model, states in zip(models, labels):
             learner.add_data(states, model)
+
+    def test_gradient(self):
+        weights = np.zeros(24)
+        learner = Learner(MatrixBeliefPropagator)
+        self.set_up_learner(learner)
+        learner.set_regularization(0.0, 1.0)
+        gradient_error = check_grad(learner.subgrad_obj, learner.subgrad_grad, weights)
+
+        # numerical_grad = approx_fprime(weights, learner.subgrad_obj, 1e-4)
+        # analytical_grad = learner.subgrad_grad(weights)
+        # plt.plot(numerical_grad, 'r')
+        # plt.plot(analytical_grad, 'b')
+        # plt.show()
+
+        print("Gradient error: %f" % gradient_error)
+        assert gradient_error < 1e-1, "Gradient is wrong"
+
+    def test_m_step_gradient(self):
+        weights = np.zeros(24)
+        learner = EM(MatrixBeliefPropagator)
+        self.set_up_learner(learner)
+        learner.set_regularization(0.0, 1.0)
+        learner.e_step(weights)
+        gradient_error = check_grad(learner.objective, learner.gradient, weights)
+
+        # numerical_grad = approx_fprime(weights, learner.objective, 1e-4)
+        # analytical_grad = learner.gradient(weights)
+        # plt.plot(numerical_grad, 'r')
+        # plt.plot(analytical_grad, 'b')
+        # plt.show()
+
+        print("Gradient error: %f" % gradient_error)
+        assert gradient_error < 1e-1, "Gradient is wrong"
 
     def test_learner(self):
         weights = np.zeros(24)
@@ -86,7 +117,7 @@ class TestLearner(unittest.TestCase):
             new_obj = learner.subgrad_obj(learner.weight_record[i, :])
             assert new_obj >= 0, "Paired dual objective was not non-negative"
 
-    def create_model(self,num_states,d):
+    def create_random_model(self, num_states, d):
         model = LogLinearModel()
 
         model.declare_variable(0, num_states)
@@ -106,5 +137,5 @@ class TestLearner(unittest.TestCase):
         model.set_edge_factor((0, 1), np.zeros((num_states, num_states)))
         model.set_edge_factor((1, 2), np.zeros((num_states, num_states)))
 
-        return  model
+        return model
 