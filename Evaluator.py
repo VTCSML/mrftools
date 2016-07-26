fixed some bugs in image loader, changed PairedDual to use scipy minimize, fixing the fixed-point-test. Changed gradient-inference-learning-tests

Evaluator.py
@@ -50,9 +50,9 @@ class Evaluator(object):
                 print("Error rate: %f" % error_rate)
                 print("Baseline from guessing all background: %f" % baseline_rate)
                 if inc == "true":
-                    inconsistency = bp._compute_inconsistency_vector()
+                    inconsistency = bp.compute_inconsistency()
                     total_inconsistency += inconsistency
-                    print("inconsistency of %s: %f" % (names[i], np.sum(inconsistency)))
+                    print("inconsistency of %s: %f" % (names[i], inconsistency))
 
                 average_errors += error_rate
 

GradientInferenceLearner.py
@@ -26,7 +26,7 @@ class GradientInferenceLearner(Learner):
             self.bp_list.append(bp)
             self.message_start.append(self.message_start[-1] + bp.message_mat.size)
 
-    def learn(self, weights, callback_f):
+    def learn(self, weights, callback_f=None):
         weights_messages = np.zeros(self.message_start[-1])
         weights_messages[:self.weight_dim] = weights
 

ImageLoader.py
@@ -55,12 +55,13 @@ class ImageLoader(object):
         plt.xlabel("Labels")
         plt.show()
 
-    def load_all_images_and_labels(self, directory, num_states, num_images):
+    def load_all_images_and_labels(self, directory, num_states, num_images=np.inf):
         images = []
         models = []
         labels = []
         names = []
         files = [x for x in os.listdir(directory) if x.endswith(".jpg") or x.endswith('.png')]
+        num_images = min(len(files), num_images)
         start = time.time()
         for i, filename in enumerate(files):
             if i < num_images:

tests/test_gradient_inference_learner.py
@@ -54,21 +54,18 @@ class TestGradientInferenceLearner(unittest.TestCase):
 
         return model
 
-    def test_gradient(self):
-        weights_messages = 0.05 * np.random.randn(8 + 32 + 64)
+    def test_message_convergence(self):
+        weights = 0.05 * np.random.randn(8 + 32)
         learner = GradientInferenceLearner(MatrixBeliefPropagator)
         self.set_up_learner(learner)
         learner.set_regularization(0.0, 1.0)
-        gradient_error = check_grad(learner.dual_obj, learner.dual_grad, weights_messages)
+        learner.learn(weights)
 
-        # numerical_grad = approx_fprime(weights_messages, learner.dual_obj, 1e-6)
-        # analytical_grad = learner.dual_grad(weights_messages)
-        # plt.plot(numerical_grad, 'r')
-        # plt.plot(analytical_grad, 'b')
-        # plt.show()
+        for bp in learner.belief_propagators:
+            bp.compute_beliefs()
+            bp.compute_pairwise_beliefs()
 
-        print("Gradient error: %f" % gradient_error)
-        assert gradient_error < 1e-1, "Gradient is wrong"
+            assert bp.compute_inconsistency() < 1e-2, "Belief propagator was not consistent"
 
 
     def test_gradient_learner(self):