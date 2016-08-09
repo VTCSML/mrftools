541c8645fde8f5c4c599bc84d1a05a3afb2f2276
bert <bhuang@vt.edu>
2016-08-09 16:31:25 -0400

    used autograd make_grad_dot to compute gradient

--- mrftools/test_sparse_dot.py
+++ mrftools/test_sparse_dot.py
@@ -2,7 +2,11 @@ import autograd.numpy as np
 from autograd import grad
 from autograd.core import primitive
 from autograd.core import getval
+from autograd.numpy.numpy_grads import make_grad_dot
 from scipy.sparse import csc_matrix
+from scipy.optimize import check_grad
+import unittest
+
 
 x = np.array([[0.50578627, 0.37340201, 0.52078787],
               [0.96267211, 0.5459987, 0.18807968]])
@@ -17,34 +21,19 @@ y = csc_matrix((data, (row, col)))
 print x.shape
 print y.shape
 
-
 @primitive
 def sparse_dot(full_matrix, sparse_matrix):
     return sparse_matrix.T.dot(full_matrix.T).T
 
-sparse_dot.defgrad(lambda ans, full_matrix, sparse_matrix : unbroadcast(ans, full_matrix, lambda g :   g * sparse_matrix))
-#sparse_dot.defgrad(lambda ans, full_matrix, sparse_matrix : unbroadcast(ans, sparse_matrix, lambda g : - g * full_matrix * sparse_matrix), argnum=1)
+print y.todense()
 
-isarray = lambda x : isinstance(getval(x), np.ndarray)
+sparse_dot.defgrad(lambda ans, full_matrix, sparse_matrix : make_grad_dot(0, ans, full_matrix, sparse_matrix.todense()))
 
-def unbroadcast(ans, x, gradfun):
-    # x is the argument that we're differentiating with respect to.
-    if isarray(x):
-        shape = x.shape
-        def new_fun(g):
-            result = gradfun(g)
-            while np.ndim(result) > len(shape):
-                result = np.sum(result, axis=0)
-            for axis, size in enumerate(shape):
-                if size == 1:
-                    result = np.sum(result, axis=axis, keepdims=True)
-            assert np.shape(result) == shape
-            return result
-    elif isarray(ans):
-        new_fun = lambda g : np.sum(gradfun(g))
-    else:
-        return gradfun
-    new_fun.__name__ = "unbroadcast_{0}".format(gradfun.__name__)
-    return new_fun
+objective = lambda x: np.sum(sparse_dot(x.reshape((2, 3)), y))
 
-print grad(lambda x, y: np.sum(sparse_dot(x, y)))(x, y)
+gradient = grad(objective)
+
+print gradient(x.ravel())
+
+print check_grad(objective, gradient, x.ravel())
+
