53954170b9a24205f4c8041b7eaae2edd8e533f1
elahehraisi <maman>
2016-08-23 16:30:03 -0400

    added adagraph instead of minimize

--- mrftools/PairedDual.py
+++ mrftools/PairedDual.py
@@ -15,9 +15,13 @@ class PairedDual(Learner):
         self.bp_iter = bp_iter
 
     def learn(self, weights, callback_f=None):
-        self.set_inference_truncation(self.bp_iter)
-        res = minimize(self.dual_obj, weights, method='L-BFGS-B', jac=self.subgrad_grad, callback=callback_f)
-        new_weights = res.x
+        for bp in self.belief_propagators + self.belief_propagators_q:
+            bp.set_max_iter(self.bp_iter)
+        # res = minimize(self.dual_obj, weights, method='L-BFGS-B', jac=self.subgrad_grad, callback=callback_f)
+        # new_weights = res.x
+
+        res = ada_grad(self.dual_obj, self.subgrad_grad, weights, args= None, callback= callback_f)
+        new_weights = res
         return new_weights
 
     def dual_obj(self, weights, options=None):
