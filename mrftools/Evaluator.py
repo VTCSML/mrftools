fixed evaluator average accuracy bug

mrftools/Evaluator.py
@@ -95,7 +95,7 @@ class Evaluator(object):
 
                 average_errors += error_rate
 
-            average_errors = np.true_divide(average_errors, i + 1)
+        average_errors = np.true_divide(average_errors, num_images)
         if inc == True:
             print("Overall inconsistency: %f" % total_inconsistency)
             return average_errors, total_inconsistency