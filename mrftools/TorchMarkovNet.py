Added in Variables for attempt to do automatic gradients via PyTorch (technically works but Sparse x Full in Variables is not yet implemented). Fixed issue where some computers would fail tests because the conditions were not initializing to zero but instead filled with random numbers Added and modified tests for 'initializing' CUDA which contributes a non-insignificant time to the first run of every test otherwise

mrftools/TorchMarkovNet.py
@@ -1,11 +1,12 @@
 """Markov network class for storing potential functions and structure."""
 import torch as t
+from torch.autograd import Variable
 
 
 class TorchMarkovNet(object):
     """Object containing the definition of a pairwise Markov net."""
 
-    def __init__(self, is_cuda):
+    def __init__(self, is_cuda, var_on):
         """Initialize a Markov net."""
         self.edge_potentials = dict()
         self.unary_potentials = dict()
@@ -28,6 +29,7 @@ class TorchMarkovNet(object):
         self.message_index = None
         self.degrees = None
         self.is_cuda = is_cuda
+        self.var_on = var_on
 
     def set_unary_factor(self, variable, potential):
         """
@@ -127,6 +129,8 @@ class TorchMarkovNet(object):
         """
         assert t.eq(self.unary_mat.size(), unary_mat.size())
         self.unary_mat[:, :] = unary_mat
+        if self.var_on:
+            self.unary_mat[:, :] = Variable(unary_mat, requires_grad=self.var_on)
         if self.is_cuda:
             self.unary_mat = self.unary_mat.cuda()
 
@@ -154,10 +158,15 @@ class TorchMarkovNet(object):
 
         self.max_states = max([len(x) for x in self.unary_potentials.values()])
         self.unary_mat = -float('inf') * t.ones(self.max_states, len(self.variables)).double()
+        if self.var_on:
+            self.unary_mat = Variable(-float('inf') * t.ones(self.max_states, len(self.variables)).double(),
+                                      requires_grad=self.var_on)
         if self.is_cuda:
             self.unary_mat = self.unary_mat.cuda()
 
-        self.degrees = t.DoubleTensor(len(self.variables)).zero_()
+        self.degrees =t.DoubleTensor(len(self.variables)).zero_()
+        if self.var_on:
+            self.degrees = Variable(t.DoubleTensor(len(self.variables)).zero_(), requires_grad=self.var_on)
         if self.is_cuda:
             self.degrees = self.degrees.cuda()
 
@@ -182,6 +191,10 @@ class TorchMarkovNet(object):
                     self.num_edges += 1
 
         self.edge_pot_tensor = -float('inf') * t.ones(self.max_states, self.max_states, 2 * self.num_edges).double()
+        if self.var_on:
+            self.edge_pot_tensor = Variable(
+                -float('inf') * t.ones(self.max_states, self.max_states, 2 * self.num_edges).double(),
+                requires_grad=self.var_on)
         if self.is_cuda:
             self.edge_pot_tensor = self.edge_pot_tensor.cuda()
 
@@ -238,19 +251,30 @@ class TorchMarkovNet(object):
             t.ones(len(to_rows)).double(),
             t.Size([2 * self.num_edges, len(self.variables)])
         )
+        if self.var_on:
+            # ISSUE: Sparse Variable ops not yet implemented
+            self.message_to_map = Variable(t.sparse.DoubleTensor(
+                t.LongTensor([to_rows, to_cols]),
+                t.ones(len(to_rows)).double(),
+                t.Size([2 * self.num_edges, len(self.variables)])
+            ), requires_grad=self.var_on)
         if self.is_cuda:
             self.message_to_map = self.message_to_map.cuda()
 
         # store an array that lists which variable each message is sent to
         self.message_to = t.LongTensor(2 * self.num_edges).zero_()
         self.message_to[t.LongTensor(to_rows)] = t.LongTensor(to_cols)
+        if self.var_on:
+            self.message_to = Variable(t.LongTensor(2 * self.num_edges).zero_(), requires_grad=self.var_on)
         if self.is_cuda:
             self.message_to = self.message_to.cuda()
-            self.message_to[t.LongTensor(to_rows).cuda()] = t.LongTensor(to_cols)
+            self.message_to[t.LongTensor(to_rows).cuda()] = t.LongTensor(to_cols).cuda()
 
         # store an array that lists which variable each message is received from
         self.message_from = t.LongTensor(2 * self.num_edges).zero_()
         self.message_from[t.LongTensor(from_rows)] = t.LongTensor(from_cols)
+        if self.var_on:
+            self.message_from = Variable(t.LongTensor(2 * self.num_edges).zero_(), requires_grad=self.var_on)
         if self.is_cuda:
             self.message_from = self.message_from.cuda()
-            self.message_from[t.LongTensor(from_rows).cuda()] = t.LongTensor(from_cols)
+            self.message_from[t.LongTensor(from_rows).cuda()] = t.LongTensor(from_cols).cuda()

mrftools/TorchMatrixBeliefPropagator.py
@@ -2,6 +2,7 @@
 # import numpy as np
 from Inference import Inference
 import torch as t
+from torch.autograd import Variable
 '''
 https://github.com/pytorch/pytorch/issues/1668
 if torch.cuda.available():
@@ -16,7 +17,7 @@ class TorchMatrixBeliefPropagator(Inference):
     indexing underlying belief propagation.
     """
 
-    def __init__(self, markov_net, is_cuda):
+    def __init__(self, markov_net, is_cuda, var_on):
         """
         Initialize belief propagator for markov_net.
 
@@ -25,6 +26,7 @@ class TorchMatrixBeliefPropagator(Inference):
         """
         self.mn = markov_net
         self.is_cuda = is_cuda
+        self.var_on = var_on
         self.var_beliefs = dict()
         self.pair_beliefs = dict()
 
@@ -38,10 +40,18 @@ class TorchMatrixBeliefPropagator(Inference):
         self.initialize_messages()
 
         self.belief_mat = t.DoubleTensor(self.mn.max_states, len(self.mn.variables)).zero_()
+        if self.var_on:
+            self.belief_mat = Variable(t.DoubleTensor(self.mn.max_states, len(self.mn.variables)).zero_(),
+                                       requires_grad=self.var_on)
         if self.is_cuda:
             self.belief_mat = self.belief_mat.cuda()
 
+
         self.pair_belief_tensor = t.DoubleTensor(self.mn.max_states, self.mn.max_states, self.mn.num_edges).zero_()
+        if self.var_on:
+            self.pair_belief_tensor = Variable(
+                t.DoubleTensor(self.mn.max_states, self.mn.max_states, self.mn.num_edges).zero_(),
+                requires_grad=self.var_on)
         if self.is_cuda:
             self.pair_belief_tensor = self.pair_belief_tensor.cuda()
 
@@ -49,13 +59,16 @@ class TorchMatrixBeliefPropagator(Inference):
 
         # the augmented_mat is used to condition variables or for loss-augmented inference for max-margin learning
         self.augmented_mat = t.DoubleTensor(self.mn.max_states, len(self.mn.variables)).zero_()
+        if self.var_on:
+            self.augmented_mat = Variable(t.DoubleTensor(self.mn.max_states, len(self.mn.variables)).zero_(),
+                                          requires_grad=self.var_on)
         if self.is_cuda:
             self.augmented_mat = self.augmented_mat.cuda()
 
         self.fully_conditioned = False  # true if every variable has been conditioned
 
         # conditioned stores the indices of variables that have been conditioned, initialized to all False
-        self.conditioned = t.ByteTensor(len(self.mn.variables))
+        self.conditioned = t.ByteTensor(len(self.mn.variables)).zero_()
         if self.is_cuda:
             self.conditioned = self.conditioned.cuda()
 
@@ -77,6 +90,9 @@ class TorchMatrixBeliefPropagator(Inference):
         :return: None
         """
         self.message_mat = t.DoubleTensor(self.mn.max_states, 2 * self.mn.num_edges).zero_()
+        if self.var_on:
+            self.message_mat = Variable(t.DoubleTensor(self.mn.max_states, 2 * self.mn.num_edges).zero_(),
+                                        requires_grad=self.var_on)
         if self.is_cuda:
             self.message_mat = self.message_mat.cuda()
 
@@ -359,6 +375,29 @@ class TorchMatrixBeliefPropagator(Inference):
         assert t.eq(self.message_mat.size(), messages.size())
         self.message_mat = messages
 
+    def autograd_unary(self):
+        """
+        Computes the backward pass over the network and returns the gradient of the unary beliefs
+        :return: gradient of unary beliefs
+        """
+        if self.var_on:
+            self.belief_mat.backward()
+            return self.belief_mat.grad
+        else:
+            print "Variables flag (var_on) not set to True to allow for autograd"
+            return
+
+    def autograd_pairwise(self):
+        """
+        Computes the backward pass over the network and returns the gradient of the pairwise beliefs
+        :return: gradient of pairwise beliefs
+        """
+        if self.var_on:
+            self.pair_beliefs.backward()
+            return self.pair_beliefs.grad
+        else:
+            print "Variables flag (var_on) not set to True to allow for autograd"
+            return
 
 def torch_logsumexp(matrix, dim=None):
     """
@@ -391,6 +430,9 @@ def sparse_dot(full_matrix, sparse_matrix):
     """
     Convenience function to compute the dot product of a full matrix and a sparse matrix.
     Useful to avoid writing code with a lot of transposes.
+
+    ISSUE: Sparse operations on Variables not yet supported (is supported for Tensors)
+    https://github.com/pytorch/pytorch/issues/2389
 
     :param full_matrix: dense matrix
     :type full_matrix: ndarray

test/test_torch_markov_net.py
@@ -9,7 +9,7 @@ class TestTorchMarkovNet(unittest.TestCase):
     """Test class for the MarkovNet model objects"""
     def create_chain_model(self, is_cuda):
         """Create chain model with different variable cardinalities."""
-        mn = TorchMarkovNet(is_cuda=is_cuda)
+        mn = TorchMarkovNet(is_cuda=is_cuda, var_on=False)
 
         np.random.seed(1)
 
@@ -34,7 +34,7 @@ class TestTorchMarkovNet(unittest.TestCase):
 
     def test_structure(self):
         """Test that the structure of the MarkovNet is properly set up"""
-        mn = TorchMarkovNet(is_cuda=False)
+        mn = TorchMarkovNet(is_cuda=False, var_on=False)
 
         mn.set_unary_factor(0, torch.from_numpy(np.random.randn(4)))
         mn.set_unary_factor(1, torch.from_numpy(np.random.randn(3)))

test/test_torch_matrix_belief_propagator.py
@@ -8,7 +8,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
     """Test class for TorchMatrixBeliefPropagator"""
     def create_chain_model(self, is_cuda):
         """Create chain MRF with variable of different cardinalities."""
-        mn = TorchMarkovNet(is_cuda=is_cuda)
+        mn = TorchMarkovNet(is_cuda=is_cuda, var_on=False)
 
         np.random.seed(1)
 
@@ -91,7 +91,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
 
     def create_grid_model(self, is_cuda, my_l, my_k):
         """Create a grid-structured MRF"""
-        mn = TorchMarkovNet(is_cuda=is_cuda)
+        mn = TorchMarkovNet(is_cuda=is_cuda, var_on=False)
         np.random.seed(1)
 
         length = my_l
@@ -138,7 +138,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
 
     def create_grid_model_simple_edges(self, is_cuda):
         """Create a grid-structured MRFs with edge potentials that are attractive."""
-        mn = TorchMarkovNet(is_cuda=is_cuda)
+        mn = TorchMarkovNet(is_cuda=is_cuda, var_on=False)
         np.random.seed(1)
 
         length = 2
@@ -187,7 +187,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
         """Test that Matrix BP produces the true marginals in a chain model."""
         is_cuda = False
         mn = self.create_chain_model(is_cuda=is_cuda)
-        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda)
+        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda, var_on=False)
         bp.infer(display='full')
         bp.load_beliefs()
 
@@ -219,7 +219,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
         is_cuda = False
         mn = self.create_loop_model(is_cuda=is_cuda)
 
-        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda)
+        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda, var_on=False)
         bp.infer(display='full')
 
         bp.load_beliefs()
@@ -240,7 +240,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
         is_cuda = False
         mn = self.create_loop_model(is_cuda=is_cuda)
 
-        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda)
+        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda, var_on=False)
         bp.infer(display='full')
 
         bp.load_beliefs()
@@ -257,7 +257,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
         is_cuda = False
         mn = self.create_loop_model(is_cuda=is_cuda)
 
-        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda)
+        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda, var_on=False)
 
         bp.condition(2, 0)
 
@@ -285,7 +285,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
 
         mn.create_matrices()
 
-        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda)
+        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda, var_on=False)
 
         with np.errstate(all='raise'):
             bp.infer()
@@ -295,7 +295,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
         """Test that matrix BP infers consistent marginals on a grid MRF"""
         is_cuda = False
         mn = self.create_grid_model(is_cuda=is_cuda, my_l=64, my_k=8)
-        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda)
+        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda, var_on=False)
         bp.infer(display='full')
 
         bp.load_beliefs()
@@ -313,7 +313,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
         mn = self.create_grid_model(is_cuda=is_cuda, my_l=64, my_k=8)
         old_mn = self.create_grid_model_old(my_l=64, my_k=8)
 
-        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda)
+        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda, var_on=False)
         old_bp = MatrixBeliefPropagator(old_mn)
 
         bp.set_max_iter(1000)
@@ -355,7 +355,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
         bp = BeliefPropagator(model_old)
         bp.load_beliefs()
 
-        mat_bp = TorchMatrixBeliefPropagator(markov_net=model, is_cuda=is_cuda)
+        mat_bp = TorchMatrixBeliefPropagator(markov_net=model, is_cuda=is_cuda, var_on=False)
         mat_bp.load_beliefs()
 
         for i in range(4):
@@ -522,26 +522,33 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
 
 
     def test_gpu_speedup(self):
-        self.my_l = 64
+        self.my_l = 8
         self.my_k = 8
         slow = True
 
         try:
+            # CUDA must be "started up" in order to achieve max speeds
+            # Approximate loss of X seconds on first run otherwise
+            init_mn = self.create_grid_model(is_cuda=True, my_l=2, my_k=2)
+            init_bp = TorchMatrixBeliefPropagator(markov_net=init_mn, is_cuda=True, var_on=False)
+            init_bp.set_max_iter(1000)
+            init_bp.infer(display='off')
+
             t_prime0 = time.time()
 
             cuda_mn = self.create_grid_model(is_cuda=True, my_l=self.my_l, my_k=self.my_k)
-            cuda_bp = TorchMatrixBeliefPropagator(markov_net=cuda_mn, is_cuda=True)
+            cuda_bp = TorchMatrixBeliefPropagator(markov_net=cuda_mn, is_cuda=True, var_on=False)
             cuda_bp.set_max_iter(1000)
             t0 = time.time()
-            cuda_bp.infer(display='final')
+            cuda_bp.infer(display='off')
             t1 = time.time()
             cuda_bp_time = t1-t0
 
             mn = self.create_grid_model(is_cuda=False, my_l=self.my_l, my_k=self.my_k)
-            bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=False)
+            bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=False, var_on=False)
             bp.set_max_iter(1000)
             t0 = time.time()
-            bp.infer(display='final')
+            bp.infer(display='off')
             t1 = time.time()
             bp_time = t1 - t0
 
@@ -549,7 +556,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
             old_bp = MatrixBeliefPropagator(old_mn)
             old_bp.set_max_iter(1000)
             t0 = time.time()
-            old_bp.infer(display='final')
+            old_bp.infer(display='off')
             t1 = time.time()
             old_bp_time = t1 - t0
 
@@ -557,7 +564,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
                 slow_bp = BeliefPropagator(old_mn)
                 slow_bp.set_max_iter(1000)
                 t0 = time.time()
-                slow_bp.infer(display='final')
+                slow_bp.infer(display='off')
                 t1 = time.time()
                 slow_bp_time = t1 - t0
 
@@ -603,6 +610,13 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
         slow = True
 
         try:
+            # CUDA must be "started up" in order to achieve max speeds
+            # Approximate loss of X seconds on first run otherwise
+            init_mn = self.create_grid_model(is_cuda=True, my_l=2, my_k=2)
+            init_bp = TorchMatrixBeliefPropagator(markov_net=init_mn, is_cuda=True, var_on=False)
+            init_bp.set_max_iter(1000)
+            init_bp.infer(display='off')
+
             t_total0 = time.time()
             print("k = %d" % self.my_k)
             if slow:
@@ -614,7 +628,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
                 t_prime0 = time.time()
 
                 cuda_mn = self.create_grid_model(is_cuda=True, my_l=self.my_l, my_k=self.my_k)
-                cuda_bp = TorchMatrixBeliefPropagator(markov_net=cuda_mn, is_cuda=True)
+                cuda_bp = TorchMatrixBeliefPropagator(markov_net=cuda_mn, is_cuda=True, var_on=False)
                 cuda_bp.set_max_iter(1000)
 
                 t0 = time.time()
@@ -623,7 +637,7 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
                 cuda_bp_time = t1 - t0
 
                 mn = self.create_grid_model(is_cuda=False, my_l=self.my_l, my_k=self.my_k)
-                bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=False)
+                bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=False, var_on=False)
                 bp.set_max_iter(1000)
                 t0 = time.time()
                 bp.infer(display='off')
@@ -678,3 +692,42 @@ class TestTorchMatrixBeliefPropagator(unittest.TestCase):
         except AssertionError:
             print "\n\nCUDA was not found within your PyTorch package\n\n"
             assert True
+
+    def test_cuda_time_loss(self):
+        self.my_l = 8
+        self.my_k = 8
+        try:
+            init_mn = self.create_grid_model(is_cuda=True, my_l=self.my_l, my_k=self.my_k)
+            init_bp = TorchMatrixBeliefPropagator(markov_net=init_mn, is_cuda=True, var_on=False)
+            init_bp.set_max_iter(1000)
+            init_time0 = time.time()
+            init_bp.infer(display='off')
+            init_time1 = time.time()
+
+            init_time = init_time1 - init_time0
+
+            cuda_mn = self.create_grid_model(is_cuda=True, my_l=self.my_l, my_k=self.my_k)
+            cuda_bp = TorchMatrixBeliefPropagator(markov_net=cuda_mn, is_cuda=True, var_on=False)
+            cuda_bp.set_max_iter(1000)
+            t0 = time.time()
+            cuda_bp.infer(display='off')
+            t1 = time.time()
+            cuda_bp_time = t1 - t0
+
+            time_diff = init_time - cuda_bp_time
+            print("Total time lost to CUDA initialization %f" % (time_diff))
+            print("In this case, this is a factor of %f times slower" % (time_diff/cuda_bp_time))
+
+        except AssertionError:
+            print "\n\nCUDA was not found within your PyTorch package\n\n"
+            assert True
+
+
+    def test_autograd(self):
+        is_cuda = True
+        mn = self.create_grid_model(is_cuda=is_cuda, my_l=64, my_k=8)
+        bp = TorchMatrixBeliefPropagator(markov_net=mn, is_cuda=is_cuda, var_on=False)
+        bp.infer(display='full')
+
+        bp.load_beliefs()
+        print bp.autograd_unary()