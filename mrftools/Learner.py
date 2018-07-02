8b01ba0686c1d9ba8149f174b7ec26098f7565bf
yolu1055 <yolu1055@colorado.edu>
2018-07-02 16:40:16 +0800

    Fix the bug about computing the objective function value. That is, when using l2 norm, we should multipy the regularizer with 0.5

--- mrftools/Learner.py
+++ mrftools/Learner.py
@@ -26,7 +26,8 @@ class Learner(object):
         self.conditioned_belief_propagators = []
         self.belief_propagators = []
         self.l1_regularization = 0.00
-        self.l2_regularization = 1e-2
+        self.l2_regularization = 1e-2 # real dataset
+        #self.l2_regularization = 1e-1 #synthetic chain
         self.weight_dim = None
         self.fully_observed = True
         self.initialization_flag = False
@@ -190,7 +191,7 @@ class Learner(object):
                                                                   do_inference)
         return self.gradient(weights)
 
-    def learn(self, weights, output_dir, optimizer=sgd, callback=None, opt_args=None):
+    def learn(self, weights, output_dir, optimizer=ada_grad, callback=None, opt_args=None):
         """
         Fit model parameters my maximizing the variational likelihood
         :param weights: Initial weight vector. Can be used to warm start from a previous solution.
@@ -260,7 +261,7 @@ class Learner(object):
         objective = 0.0
         # add regularization penalties
         objective += self.l1_regularization * np.sum(np.abs(weights))
-        objective += self.l2_regularization * weights.dot(weights)
+        objective += 0.5 * self.l2_regularization * weights.dot(weights)
         objective += self.term_q_p
 
         return objective
@@ -368,7 +369,7 @@ class Learner(object):
         objective = 0.0
         # add regularization penalties
         objective += self.l1_regularization * np.sum(np.abs(weights))
-        objective += self.l2_regularization * weights.dot(weights)
+        objective += 0.5 * self.l2_regularization * weights.dot(weights)
         objective += self.term_q_p
 
         return objective
